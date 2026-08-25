82aa9b783af6b9fea523a52347804f8bc23a4cb7
Navya1527 <kanugulanavya1527@gmail.com>
2026-08-25 10:07:29 +0530

    Revert "code good"
    This reverts commit 3ebb716f52be0c6eb3b9f93c6176220209ff37e2.

--- src/test/java/tests/ZoomLobbyManagementTest.java
+++ src/test/java/tests/ZoomLobbyManagementTest.java
@@ -105,7 +105,6 @@ public class ZoomLobbyManagementTest extends BaseTest {
 
         System.out.println(
                 "✓ Zoom Admit button clicked");
-        Thread.sleep(5000);
 
 // --------------------------------------------------------
 // Step 4 - Verify participant count after Admit
